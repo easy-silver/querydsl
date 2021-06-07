aa5812a18f7e549052a618898b5546452c1dc55a
Timo <developer.timo@gmail.com>
2021-06-08 01:00:38 +0900

    Member, Team 엔티티 추가 및 검증

---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,32 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@ToString(of = {"id", "name"})
+@Getter @Setter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+public class Team {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
+
