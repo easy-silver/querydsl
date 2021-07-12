ea9e35cc09b11cbe8ff71b90a25eb255fec25ca5
Timo <developer.timo@gmail.com>
2021-07-12 20:49:00 +0900

    조회 API 컨트롤러 개발

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -107,7 +107,9 @@ public class MemberJpaRepository {
                 .where(
                         usernameEq(condition.getUsername()),
                         teamNameEq(condition.getTeamName()),
-                        ageBetween(condition.getAgeGoe(), condition.getAgeLoe())
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                        //ageBetween(condition.getAgeGoe(), condition.getAgeLoe())
                 )
                 .fetch();
     }
